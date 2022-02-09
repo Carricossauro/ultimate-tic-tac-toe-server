7a882f8fd1e12046ca67326c055991955744fc27
Tiago Carriço <tiagocarrico27@gmail.com>
2022-02-09 19:18:21 +0000

    express added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { MongoClient, ObjectId } from "mongodb";
 import { Server } from "socket.io";
 import "dotenv/config";
+import express from "express";
 
 const smallBoardEmpty = ["", "", "", "", "", "", "", "", ""];
 const bigBoardEmpty = [
@@ -220,9 +221,16 @@ Socket.io only from here and beyond
 ###################################
 */
 
-const io = new Server(process.env.PORT, {
+const PORT = process.env.PORT || 3000;
+const INDEX = "/index.html";
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = new Server(server, {
     cors: {
-        origin: "*:*",
+        origin: "*",
     },
 });
 
